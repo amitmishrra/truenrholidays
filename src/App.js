9fe4afe000be7013d6368732eb584ae19995c64a
mishra4amit4@gmail.com <mishra4amit4@gmail.com>
2022-12-26 07:59:44 +0530

    paths fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom"
 import CommonContainer from "./CommanContainer";
+import Destination from "./Components/Destination";
 import HomePage from "./Pages/HomePage";
 import DomesticPackages from "./Pages/PackagesPage/Domestic";
 import InternationalPackages from "./Pages/PackagesPage/International";
@@ -27,9 +28,17 @@ function App() {
           <Route path="/internationalPackages"
             element={
               <CommonContainer>
-                <InternationalPackages/>
+                <InternationalPackages />
               </CommonContainer>
             } />
+
+          <Route path="/internationalPackages/Thailand"
+            element={
+              <CommonContainer>
+                <Destination />
+              </CommonContainer>
+            } />
+
 
         </Routes>
 

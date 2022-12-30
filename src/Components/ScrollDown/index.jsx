e6ffa8eb9cb6c97d5e6491c4a6c6b3c8796570c6
mishra4amit4@gmail.com <mishra4amit4@gmail.com>
2022-12-30 07:25:36 +0530

    djfsdjk

--- src/Components/ScrollDown/index.jsx
+++ src/Components/ScrollDown/index.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./style.css"
 export default function ScrollDown() {
+    // const [isPhone, setIsPhone] = RuseState(pone);
+    // useEffect
   return (
-    <div class="arrow"></div>
+    <div onClick={()=>{window.scrollBy(0, 700);}} class="arrow"></div>
   )
 }
